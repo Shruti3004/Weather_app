cafdc7091429a036eda58b452ac42d010eee1b32
Shruti3004 <mail2shruti.ag@gmail.com>
2020-11-23 02:25:26 +0530

    Call Stack, Call Queue, Event Loop, Nodde APIs

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ setTimeout(() => {
     console.log('0 second timer');
 }, 0);
 console.log('Stopping');
+console.log('Stopping');
+console.log('Stopping');
+console.log('Stopping');
+console.log('Stopping');
+console.log('Stopping');
+console.log('Stopping');
+console.log('Stopping');
+
+// Output
+// Starting
+// Stopping
+// Stopping
+// Stopping
+// Stopping
+// Stopping
+// Stopping
+// Stopping
+// Stopping
+// 0 second timer
+// 2 second timer
