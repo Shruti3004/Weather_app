8f993f5f5246c7306167730c4596d97299dcaa88
Shruti3004 <mail2shruti.ag@gmail.com>
2020-11-23 20:16:12 +0530

    Making HTTp requests

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-console.log('Starting');
-setTimeout(() => {
-    console.log('2 second timer');
-}, 2000);
-setTimeout(() => {
-    console.log('0 second timer');
-}, 0);
-console.log('Stopping');
-console.log('Stopping');
-console.log('Stopping');
-console.log('Stopping');
-console.log('Stopping');
-console.log('Stopping');
-console.log('Stopping');
-console.log('Stopping');
+const request = require('request');
 
-// Output
-// Starting
-// Stopping
-// Stopping
-// Stopping
-// Stopping
-// Stopping
-// Stopping
-// Stopping
-// Stopping
-// 0 second timer
-// 2 second timer
+const url ="http://dataservice.accuweather.com/currentconditions/v1/202396?apikey=K77ESaJGE8NH6Hy2BlfZDSFeym2cxHKG"
+
+request({url}, (error, response) => {
+    const data = JSON.parse(response.body) 
+    console.log(data[0].Temperature)
+})
+
+
+
+// console.log('Starting');
+// setTimeout(() => {
+//     console.log('2 second timer');
+// }, 2000);
+// setTimeout(() => {
+//     console.log('0 second timer');
+// }, 0);
+// console.log('Stopping');
+// console.log('Stopping');
+// console.log('Stopping');
+// console.log('Stopping');
+// console.log('Stopping');
+// console.log('Stopping');
+// console.log('Stopping');
+// console.log('Stopping');
+
+// // Output
+// // Starting
+// // Stopping
+// // Stopping
+// // Stopping
+// // Stopping
+// // Stopping
+// // Stopping
+// // Stopping
+// // Stopping
+// // 0 second timer
+// // 2 second timer
