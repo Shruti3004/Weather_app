f9cac31f87762a8213dc83b2fb518743ce7d5f3c
Shruti3004 <mail2shruti.ag@gmail.com>
2020-11-26 07:07:03 +0530

    Callbacks Abstraction

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const request = require('request');
 
-const url ="http://dataservice.accuweather.com/currentconditions/v1/202396?apikey=K77ESaJGE8NH6Hy2BlfZDSFeym2cxHKG"
+// const url ="http://dataservice.accuweather.com/currentconditions/v1/202396?apikey=K77ESaJGE8NH6Hy2BlfZDSFeym2cxHKG"
 
-request({url, json: true}, (error, response) => {  
+// request({url, json: true}, (error, response) => {  
+//     if(error){
+//         console.log('Unable to connect to the desire API')
+//     } else if(response.body.Code){
+//         console.log('Unable to find location')
+//     } else{
+//         console.log(response.body[0].Temperature)
+//     }
+// });
+
+const geocode = (address, callback) => {
+    const newUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1Ijoic2hydXRpYWdhcndhbDMwIiwiYSI6ImNraHY1bDNiczFhbmYycmxodWFoMHZxYXYifQ.TC5c58DMuOAM2MCiYwhS2A&limit=1`
+    request({url: newUrl, json: true}, (error, response) => {
+        if(error){
+            callback('Unable to connect to the desire API', undefined)
+        } else if(response.body.features.length === 0){
+            callback('Unable to find location', undefined)
+        } else{
+            callback(undefined, `${response.body.features[0].center}, ${response.body.features[0].place_name}`)
+        }
+    })
+}
+
+geocode('Varanasi, Uttar Pradesh', (error, result) => {
     if(error){
-        console.log('Unable to connect to the desire API')
-    } else if(response.body.Code){
-        console.log('Unable to find location')
+        console.log(`Error: ${error}`)
     } else{
-        console.log(response.body[0].Temperature)
-    }
-});
-
-const newUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Pilibhit.json?access_token=pk.eyJ1Ijoic2hydXRpYWdhcndhbDMwIiwiYSI6ImNraHY1bDNiczFhbmYycmxodWFoMHZxYXYifQ.TC5c58DMuOAM2MCiYwhS2A&limit=1"
-
-request({url: newUrl, json: true}, (error, response) => {
-    if(error){
-        console.log('Unable to connect to the desire API')
-    } else if(response.body.features.length === 0){
-        console.log('Unable to find location')
-    } else{
-        console.log(response.body.features[0].center)
+        console.log(`Data: ${result}`)
     }
 })
 
