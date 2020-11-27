81c67c0bf2ff4d2e011470423620620b63eabc4c
Shruti3004 <mail2shruti.ag@gmail.com>
2020-11-28 03:38:40 +0530

    Serving up html and json

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,19 +2,30 @@ const express =  require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Server Running');
+    res.send('<h1>Server Running</h1>');
 });
 
+// express identifies the object and stringify it as json
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send([{
+        name: 'Shruti',
+        age: 19
+    },
+    {
+        name: 'Andrew',
+        age: 27
+    }])
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h3>About page</h3>')
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        location: 'Boston',
+        forecast: 50
+    })
 });
 // app.com
 // app.com/help
