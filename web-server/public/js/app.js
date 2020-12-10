e5de055e46bfa1c847e30d109f5bee7e9f5f87cf
Shruti3004 <mail2shruti.ag@gmail.com>
2020-12-10 09:14:30 +0530

    HTTP request with fetch and added search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,33 @@
-console.log('client side javascript file is loaded')
+console.log('client side javascript file is loaded');
+// fetch("http://puzzle.mead.io/puzzle").then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// });
+
+// fetch("http://localhost:3000/weather?address=!").then((response) => {
+//     response.json().then((data) => {
+//         if(data.error){
+//             return console.log(data.error);
+//         }
+//         console.log(data);
+//     })
+// })
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()    
+    const location = search.value
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                return console.log(data.error);
+            }
+            console.log(data);
+        })
+    })
+})
